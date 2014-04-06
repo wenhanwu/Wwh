8f5bd0a5855b3e8f5bdd11566db9c08224a48561
Wenhan Wu <wenhanwzh@gmail.com>
2014-04-06 16:21:41 -0700

    a

---
+++ index.php
@@ -0,0 +1,50 @@
+﻿<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>无标题 1</title>
+
+<link href="dream.css" type="text/css" rel="stylesheet"/>
+<script src="now.js" type="text/javascript"></script> 
+</head>
+
+<body>
+	<div class="containerWrapper">
+		<div class="navContainer">
+			<h3 class="title">操作</h3>
+			<div class="separator-05 separator-border-2"></div>
+			<ul class="nav">
+				<li><a href="#">链接1</a></li>
+				<li class="separator-05"></li>
+				<li><a href="#">链接2</a></li>
+				<li class="separator-05"></li>
+				<li id="mememe"><a href="#">链接3</a></li>								
+			</ul>
+		</div>		
+		<div class="contentContainer">
+			<div class="contentWrapper">
+				<h3 class="title">位置：欢迎使用系统</h3>
+				<div class="separator-10"></div>
+				<h3 class="title">快速查找</h3>
+				<table class="tblQuickSearch" cellpadding="5" cellspacing="0">
+					<col width="5%"/><col width="85%"/><col width="10%"/>
+					<tr>
+						<td>合同号</td>
+						<td>
+							<div class="txtSearchOuterDiv"><div  class="txtSearchInnerDiv"><input class="txtSearch" type="text" /></div></div>
+						</td>
+						<td>
+							<div class="txtSearchOuterDiv"><input class="btn" type="submit" value="查找" onclick="here()"/></div>
+						</td>
+					</tr>
+				</table>
+				<div class="separator-10"></div>
+				<h3 class="title">房间列表</h3>
+				<div id="btnLine">
+				<div>
+				
+			</div>						
+		</div>		
+		<div class="clr"></div>
+	</div>
+</body>
+</html>
